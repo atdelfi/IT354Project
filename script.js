4ccff2e4b3df98e5644f54b3a2c09b63cd513ffa
atdelfi <60015212+atdelfi@users.noreply.github.com>
2020-05-01 03:21:16 -0500

    Added Body Paragraphs from posts

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const postsPerRequest = 4;
 const maxPostsToFetch = 20;
 const maxRequests = maxPostsToFetch / postsPerRequest;
 
+document.getElementById('subreddit').defaultValue = "news";
+
+
 const responses = [];
 
 const handleSubmit = e => {
@@ -35,15 +38,16 @@ const parseResults = responses => {
 
   statsByUser = {};
 
-  allPosts.forEach(({ data: { author, score, selftext , title, permalink } }) => {
+  allPosts.forEach(({ data: { author, score, selftext_html , title, permalink, url} }) => {
     statsByUser[author] = !statsByUser[author]
-      ? { postCount: 1, score , selftext , title ,permalink}
+      ? { postCount: 1, score , selftext_html , title ,permalink, url}
       : {
           postCount: statsByUser[author].postCount + 1,
           score: statsByUser[author].score + score,
-          selftext: statsByUser[author].selftext,
+          selftext_html: statsByUser[author].selftext_html,
           title: statsByUser[author].title,
-          permalink: statsByUser[author].permalink
+          permalink: statsByUser[author].permalink,
+          url: statsByUser[author].url
         };
   });
 
@@ -52,8 +56,9 @@ const parseResults = responses => {
     score: statsByUser[username].score,
     postCount: statsByUser[username].postCount,
     title: statsByUser[username].title,
-    selftext: statsByUser[username].selftext,
-    permalink: statsByUser[username].permalink
+    selftext_html: statsByUser[username].selftext_html,
+    permalink: statsByUser[username].permalink,
+    url: statsByUser[username].url
   }));
 
   const sortedList = userList.sort((userA, userB) => userB.score - userA.score);
@@ -63,16 +68,29 @@ const parseResults = responses => {
 
 const displayRankings = sortedList => {
   const container = document.getElementById('results-container');
-  sortedList.forEach(({ username, score, postCount, title, selftext, permalink }, i) => {
+  sortedList.forEach(({ username, score,  title, selftext_html, permalink, url }, i) => {
     rank = i + 1;
     const userCard = document.createElement('a');
+    const back = document.createElement('article')
     userCard.href = `https://www.reddit.com${permalink}`;
     userCard.classList.add('user-card');
-    userCard.innerText = `${rank}. ${username} - ${postCount} post(s) - ${score} point(s), ${title} `;
+    back.classList.add(`edit`);
+    userCard.innerText = `${rank}. ${username}  - ${score} point(s), ${title} `;
+    back.innerHTML = `${url} <br /> ${selftext_html}`;
 
     container.appendChild(userCard);
+    container.appendChild(back);
   });
 };
+const resetArea = () => {
+  const container = document.getElementById('results-container').innerHTML = "";
+  
+ responses.length = 0;
+  
+  
 
+}
 const subredditSelectForm = document.getElementById('subreddit-select-form');
 subredditSelectForm.addEventListener('submit', handleSubmit);
+subredditSelectForm.addEventListener('reset', resetArea);
+
